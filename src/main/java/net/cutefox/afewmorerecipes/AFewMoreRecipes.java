de9aff2861870ac29d1b2a4026d5152e8dd61bd9
CuteFox8302 <111240985+CuteFox8302@users.noreply.github.com>
2023-01-15 18:22:40 -0500

    Adds some recipes.

---
+++ src/main/java/net/cutefox/afewmorerecipes/AFewMoreRecipes.java
@@ -0,0 +1,14 @@
+package net.cutefox.afewmorerecipes;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AFewMoreRecipes implements ModInitializer {
+	public static final String MOD_ID = "afewmorerecipes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/cutefox/afewmorerecipes/AFewMoreRecipesClient.java
@@ -0,0 +1,10 @@
+package net.cutefox.afewmorerecipes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AFewMoreRecipesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
